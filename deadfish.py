65d534f1e9905b7575aa55d214a4d273be1c611b
sid <excectra@gmail.com>
2015-09-17 22:33:14 +0800

    fix deadfish

--- deadfish.py
+++ deadfish.py
@@ -13,7 +13,12 @@ def compile_df(dfcode):
 	accumulator = 0
 	i = 0
 	output = ""
+	signal.alarm(10)
 	while(i < len(dfcode)):
+		if variables.time_counter - variables.init_counter > 0:
+				signal.alarm(0)
+				irc.send_msg(variables.too_much_time_err, variables.channel)
+				return "[NORESULT]"
 		if dfcode[i] == 'x' or dfcode[i] == 'i':
 			accumulator += 1
 		elif dfcode[i] == 'd':
@@ -26,4 +31,6 @@ def compile_df(dfcode):
 			else:
 				output += str(accumulator) + " "
 		i += 1
+	signal.alarm(0)
+	output = irc.process_output(output)
 	return output
